6deb09a9a598621fd4ac4ab3d96081a618f652a5
alce65 <alejandro.cerezo@evosoft.es>
2020-10-29 16:17:14 +0100

    Inicio app Vue

---
+++ 02_app/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Primeros componentes que creamos</p>
+    <Saludo myName="Alejandro"></Saludo>
+  </div>
+</template>
+
+
+
+<script>
+import Saludo from './Saludo.vue'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    Saludo
+  },
+  created() {
+    console.log('Created')
+    console.log(this)
+    console.log(this.props)
+  },
+  mounted() {
+    console.log('Mounted')
+    console.log(this)
+    console.log(this.props)
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ 02_app/src/components/Saludo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- Binding en dos direcciones -->
+        <p>Hola {{usuario}}, soy {{myName}}</p>
+        <input type="text" :placeholder="inputMsg" v-model="usuario">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Saludo',
+    data: function () {
+        return {
+        usuario: 'Pepe',
+        inputMsg: 'Dime tu nombre'
+        }
+    },
+    props: {
+        myName: String
+    }
+}
+</script>
+
+<style scoped>
+    p {
+        font-size: 1.5rem;
+        color: brown;
+    }
+</style>
